ac885cdec8dbf95f738271675b6408f909870299
Peter Karda <pka@middleway.fr>
2026-03-05 14:46:01 +0100

    Initial Commit

---
+++ BusKit/BusKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusKitApp.swift
+//  BusKit
+//
+//  Created by Peter Karda on 05/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct BusKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
